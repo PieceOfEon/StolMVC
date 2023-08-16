98788027ad7273e79367ae3418b1d37569656db5
PieceOfEon <danilskripka1996@gmail.com>
2023-08-16 11:18:30 +0300

    Add project files.

---
+++ StolMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StolMVC.Models;
+using System.Diagnostics;
+
+namespace StolMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(UserModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", user);
+            }
+
+            return View("Submit", user); // Заменили "Privacy" на "Submit"
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StolMVC/Models/UserModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace StolMVC.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Поле 'ФИО' обязательно для заполнения")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Возраст' обязательно для заполнения")]
+        [Range(0, 150, ErrorMessage = "Недопустимый возраст")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Дата рождения' обязательно для заполнения")]
+        [Display(Name = "Дата рождения")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Пароль' обязательно для заполнения")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердите пароль")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Пол' обязательно для выбора")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Секретное слово' обязательно для выбора")]
+        public string SecretWord { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Ответ на секретное слово' обязательно для заполнения")]
+        [Display(Name = "Ответ на секретное слово")]
+        public string SecretWordAnswer { get; set; }
+    }
+}
